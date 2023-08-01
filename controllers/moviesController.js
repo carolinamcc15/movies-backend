4318f9cb40bcdb731c837a708f241f0fc8a0c889
Carolina Carranza <00008119@uca.edu.sv>
2023-07-31 23:53:55 -0600

    refactor: update controllers to use sequelize models' methods

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,14 +1,14 @@
-const Movies = require('../models/movies');
+const Movie = require('../models/movies');
 
 const createMovie = async (req, res) => {
   try {
-    const movie = await Movies.createMovie(req.body);
-    res.json(movie);
+    const movie = await Movie.create(req.body);
+    res.status(201).json(movie);
   } catch (error) {
     res.status(500).json({ error: 'Error al crear la película' });
   }
 };
 
 module.exports = {
-  createMovie,
+  createMovie
 };
--- controllers/optionsController.js
+++ controllers/optionsController.js
@@ -1,8 +1,8 @@
-const Options = require('../models/options');
+const Option = require('../models/options');
 
 const getOptions = async (req, res) => {
   try {
-    const options = await Options.getOptions();
+    const options = await Option.findAll();
     res.json(options);
   } catch (error) {
     res.status(500).json({ error: 'Error al obtener las opciones' });
