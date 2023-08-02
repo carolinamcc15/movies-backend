ba29996eb64a567447ddb59259b07ebafedbb2ef
Carolina Carranza <00008119@uca.edu.sv>
2023-08-02 03:05:05 -0600

    feat: add functionality to filter movies by year

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,10 +1,25 @@
+const { Op } = require('sequelize');
 const Movie = require('../models/movies');
 
 const getMovies = async (req, res) => {
+  const { year } = req.query;
+
   try {
-    const movies = await Movie.findAll();
+    let movies;
+    if (year) {
+      movies = await Movie.findAll({
+        where: {
+          date: {
+            [Op.between]: [`${year}-01-01`, `${year}-12-31`],
+          },
+        },
+      });
+    } else {
+      movies = await Movie.findAll();
+    }
     res.status(200).json(movies);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: 'Error al obtener películas' });
   }
 };
@@ -20,5 +35,5 @@ const createMovie = async (req, res) => {
 
 module.exports = {
   createMovie,
-  getMovies
+  getMovies,
 };
